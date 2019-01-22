883e55306fc17016d32260b7e9487763a3d68e58
Tiancheng-Luo <nachtmusik@163.com>
2019-01-22 12:00:49 +0800

    nested `describe` or `context`

---
+++ spec/context_spec.rb
@@ -0,0 +1,20 @@
+# `#`for instance member and `.` for class member
+RSpec.describe '#even? method' do
+    #it 'should return true if number is even'
+    #it 'should return false if number is odd'
+
+    context 'with even number' do
+    #describe 'with even number' do
+        it 'should return true' do
+            expect(6.even?).to eq(true)
+        end
+    end
+
+    context 'with odd number' do
+    #describe 'with odd number' do
+        it 'should return false' do
+            expect(5.even?).to eq(false)
+        end
+    end
+
+end
