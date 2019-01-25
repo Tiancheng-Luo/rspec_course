3534386efa5390662adc1c0999de0fed705a2e66
Tiancheng-Luo <nachtmusik@163.com>
2019-01-26 06:16:06 +0800

    use `described_class` to minimize coupling

---
+++ spec/described_class_spec.rb
@@ -0,0 +1,17 @@
+class King
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+# Change the `King` to `Royal`, minimal changes are required.
+RSpec.describe King do
+    subject {described_class.new('Boris')}
+    let(:louis) {described_class.new('Louis')}
+
+    it 'response a great person' do
+        expect(subject.name).to eq('Boris')
+    end
+end
